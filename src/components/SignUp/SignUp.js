df9125802807b0b7f811d82c2636f54e1a42070f
bikash-dev <bikash.dev.bk@gmail.com>
2022-10-22 15:08:26 +0600

    login and sign up design

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import { Link } from 'react-router-dom';
+
 
 const SignUp = () => {
     return (
-        <div>
-            <h3>This is Sign up</h3>
+        <div className='w-25 mx-auto mt-5'>
+            <Form>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Your Name</Form.Label>
+                    <Form.Control type="text" name="name" placeholder="Your name" required/>
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" name="email" placeholder="Enter email" required/>
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" name="password" placeholder="Password" required/>
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Sign Up
+                </Button>
+                <p><small>Already have an account? <Link to='/login'>Log in</Link></small></p>
+            </Form>
         </div>
     );
 };
